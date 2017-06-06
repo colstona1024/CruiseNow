0742148d2683ad6069f49360e8a602635e9e255b
colstona1024 <allencoston@Joshs-MacBook-Pro.local>
2017-06-06 16:53:46 -0400

    First commit

---
+++ Cruise Now/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Cruise Now
+//
+//  Created by Allen Colston on 5/29/17.
+//  Copyright © 2017 Allen Colston. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
